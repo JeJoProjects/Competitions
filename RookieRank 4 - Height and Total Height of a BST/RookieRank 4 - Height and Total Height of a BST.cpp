cd69a508b9ba87e62cf92bca0b3fcd6b2c22a8f0
Nice Budyy <34485687+NiceBuddyCoding@users.noreply.github.com>
2018-02-18 17:23:45 +0100

    Update RookieRank 4 - Height and Total Height of a BST.cpp

--- RookieRank 4 - Height and Total Height of a BST/RookieRank 4 - Height and Total Height of a BST.cpp
+++ RookieRank 4 - Height and Total Height of a BST/RookieRank 4 - Height and Total Height of a BST.cpp
@@ -9,8 +9,6 @@ struct node
     struct node *left, *right;
 };
 struct node *root = NULL;
-
-// A utility function to create a new BST node
 struct node *newNode(int item)
 {
     struct node *temp =  (struct node *)malloc(sizeof(struct node));
@@ -18,14 +16,10 @@ struct node *newNode(int item)
     temp->left = temp->right = NULL;
     return temp;
 }
-
 inline int treeHeight(node *root)
 {
-    // Base Case
-    if (root == NULL)
-        return 0;
-
-    // Create an empty queue for level order tarversal
+    if (root == NULL)           return 0;
+    
     queue<node *> q;
 
     // Enqueue Root and initialize height
@@ -37,21 +31,16 @@ inline int treeHeight(node *root)
         // nodeCount (queue size) indicates number of nodes
         // at current lelvel.
         int nodeCount = q.size();
-        if (nodeCount == 0)
-            return height;
-
+        if (nodeCount == 0)     return height;
         height++;
-
         // Dequeue all nodes of current level and Enqueue all
         // nodes of next level
         while (nodeCount > 0)
         {
             node *node = q.front();
             q.pop();
-            if (node->left != NULL)
-                q.push(node->left);
-            if (node->right != NULL)
-                q.push(node->right);
+            if (node->left != NULL)     q.push(node->left);
+            if (node->right != NULL)    q.push(node->right);
             nodeCount--;
         }
     }
@@ -70,45 +59,17 @@ inline struct node* findNode(struct node* node, int tofind)
     return curr;
 }
 
-
-/*int getLevelUtil(struct node *node, int key, int level)
-{
-    if (node == NULL)
-        return 0;
-
-    if (node->key == key)
-        return level;
-
-    int downlevel = getLevelUtil(node->left ,key, level+1);
-    if (downlevel != 0)
-        return downlevel;
-
-    downlevel = getLevelUtil(node->right, key, level+1);
-    return downlevel;
-}
-
-// Returns level of given data value
-int getLevel(struct node *node, int key)
-{
-    return getLevelUtil(node,key,1);
-}*/
-
 struct node* insert(struct node* node, int key)
 {
     /* If the tree is empty, return a new node */
     if (node == NULL) return newNode(key);
-
     /* Otherwise, recur down the tree */
-    if (key < node->key)
-        node->left  = insert(node->left, key);
-    else if (key > node->key)
-        node->right = insert(node->right, key);
-
+    if (key < node->key)                node->left  = insert(node->left, key);
+    else if (key > node->key)           node->right = insert(node->right, key);
     /* return the (unchanged) node pointer */
     return node;
 }
 
-// Driver Program to test above functions
 int main()
 {
     int n; cin>>n;
@@ -134,8 +95,6 @@ int main()
         int level = treeHeight(temp) - 1;
         height = max(level, height);
         sum += level;
-        //printf("Level of %d is %d\n", itr->first, level);
-        //printf("Level of %d is %d\n", itr->first, getLevel(root, itr->first));
     }
 
    cout<<height<<endl<<sum<<endl;
