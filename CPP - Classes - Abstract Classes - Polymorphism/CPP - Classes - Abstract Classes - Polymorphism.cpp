c9850b6e4a2e4656cc0e7445126ad6af6a0fd1cc
Somewhere on Earth <email2jejo@gmail.com>
2019-05-08 15:32:48 +0200

    Abstract Classes - Polymorphism: updates

--- CPP - Classes - Abstract Classes - Polymorphism/CPP - Classes - Abstract Classes - Polymorphism.cpp
+++ CPP - Classes - Abstract Classes - Polymorphism/CPP - Classes - Abstract Classes - Polymorphism.cpp
@@ -1,129 +1,135 @@
 #include <iostream>
-#include <std::vector>
-#include <std::map>
-#include <std::string>
-#include <algorithm>
-#include <std::set>
-#include <cassert>
+#include <map>
+#include <string>
+#include <cstddef>
 
 
-struct Node
+struct Node final
 {
-   Node* next;
-   Node* prev;
-   int m_value;
-   int m_key;
-   Node(Node* p, Node* n, const int& k, const int& val)
-    :prev(p),next(n),m_key(k),m_value(val)
-    {};
-   Node(const int& k, const int& val)
-    :prev(nullptr),next(nullptr),m_key(k),m_value(val)
-    {};
-    ~Node(){    delete next; delete prev;   }
+	Node* next;
+	Node* prev;
+	int _value;
+	int _key;
+
+	Node(Node* const p, Node* const n, const int k, const int val)
+		: prev{ p }
+		, next{ n }
+		, _key{ k }
+		, _value{ val }
+	{};
+
+	Node(const int k, const int val)
+		: prev{ nullptr }
+		, next{ nullptr }
+		, _key{ k }
+		, _value{ val }
+	{};
+
+	~Node()
+	{
+		std::cout << "Deleting " << _key << " " << _value << '\n';
+		delete next; next = nullptr;
+		delete prev; prev = nullptr;
+	}
 };
 
 class Cache
 {
 protected:
-   std::map<int,Node*> mp;   // std::map the key to the node in the linked list
-   int cp;              // capacity
-   Node* tail;          // double linked list tail pointer
-   Node* head;          // double linked list head pointer
-   virtual void std::set(int, int) = 0;  //std::set function
-   virtual int get(int) = 0;        //get function
+	std::map<int, Node*> mp;         // map the key to the node in the linked list
+	std::size_t cp{};                // capacity
+	Node* tail{ nullptr };           // double linked list tail pointer
+	Node* head{ nullptr };           // double linked list head pointer
+	virtual void set(const int, const int) = 0;  //set function
+	virtual int get(const int) const noexcept = 0;        //get function
 
+public:
+	Cache(const std::size_t c) : cp{ c } {}
+	virtual ~Cache()
+	{
+		for (auto& [key, value]: mp)
+		{
+			delete value;
+			value = nullptr;
+		}
+	}
 };
 
-class LRUCache: public Cache
+class LRUCache final : public Cache
 {
-    public:
-    LRUCache(int c) { cp = c; }
+public:
+	LRUCache(const std::size_t c)
+		: Cache{ c }
+	{}
 
-    void std::set(int k,int v)
-    {
-        Node *N;
+	void set(const int k, const int v) override
+	{
+		Node* N{ nullptr };
+		if (mp.empty()) //No nodes
+		{
+			N = new Node{ k, v };
+			tail = head = N;
+			mp[k] = N;
+			return;
+		}
+		if (const auto it = mp.find(k); it != mp.cend())
+		{
+			it->second->_value = v; // update value
+			if (head == it->second) return; // head! done here
 
-        if(mp.empty()) //No nodes
-        {
-            N = new Node(k,v);
-            tail = head = N;
-            mp[k] = N;
+			it->second->prev->next = it->second->next;
 
-            return;
-        }
+			if (tail == it->second) tail = tail->prev;      // tail?
+			else it->second->next->prev = it->second->prev; // in between?
 
-        auto it = mp.find(k);
+			it->second->next = head;
+			it->second->prev = nullptr;
+			head->prev = it->second;
+			head = it->second;
+		}
+		else// No in hash table
+		{
+			N = new Node{ head->prev, head, k, v }; // new node
+			head->prev = N;
+			head = N;
+			mp[k] = N;
+			if (mp.size() > cp) // capacity?
+			{
+				tail = tail->prev;
+				mp.erase(tail->next->_key);
+				delete tail->next;
+				tail->next = nullptr;
+			}
+		}
+	}
 
-        if(it != mp.end()) //In hash table
-        {
-            it->second->value = v; // update value
-
-            if(head == it->second) //head?
-                return;
-
-            it->second->prev->next = it->second->next;
-
-            if(tail == it->second) //tail?
-                tail = tail->prev;
-            else                   //in between?
-                it->second->next->prev = it->second->prev;
-
-            it->second->next = head;
-            it->second->prev = NULL;
-            head->prev = it->second;
-            head = it->second;
-        }
-        else               // No in hash table
-        {
-            N = new Node(head->prev,head,k,v); //new node
-            head->prev = N;
-            head = N;
-            mp[k] = N;
-
-            if(mp.size() > cp) //capacity?
-            {
-                tail = tail->prev;
-                mp.erase(tail->next->key);
-                delete tail->next;
-                tail->next = NULL;
-            }
-
-        }
-
-    }
-
-    int get(int k)
-    {
-        auto it = mp.find(k);
-        if(it != mp.end())
-            return it->second->value;
-
-        return -1;
-    }
-
+	int get(const int k)  const noexcept override
+	{
+		if (const auto it = mp.find(k); it != mp.cend())
+			return it->second->_value;
+		return -1;
+	}
 };
 
 int main()
 {
-    int n, capacity,i;
-    std::cin >> n >> capacity;
-    LRUCache l(capacity);
-    for(i=0;i<n;i++)
-    {
-        std::string command;
-        std::cin >> command;
-        if(command == "get")
-        {
-            int key;
-            std::cin >> key;
-            std::cout << l.get(key) << std::endl;
-        }
-        else if(command == "std::set")
-        {
-            int key, value;
-            std::cin >> key >> value;
-            l.std::set(key,value);
-        }
-   }
-   return 0;
+	std::size_t n{}, capacity{};
+	std::cin >> n >> capacity;
+	LRUCache l{ capacity };
+	while (n--)
+	{
+		std::string command{}; std::cin >> command;
+		if (command == std::string{ "get" })
+		{
+			int key{}; std::cin >> key;
+			std::cout << l.get(key) << '\n';
+		}
+		else if (command == std::string{ "set" })
+		{
+			int key{}, value{};
+			std::cin >> key >> value;
+			l.set(key, value);
+		}
+	}
+	return 0;
 }
