0847ddf020da9e112ce59edbc99ab77bf7b6b541
Somewhere on Earth <email2jejo@gmail.com>
2019-05-13 15:36:27 +0200

    AlgoStrings -  HackerRank in a String: updates

--- AlgorithmsStrings - HackerRank in a String!/AlgorithmsStrings - HackerRank in a String!.cpp
+++ AlgorithmsStrings - HackerRank in a String!/AlgorithmsStrings - HackerRank in a String!.cpp
@@ -1,38 +1,29 @@
 #include <iostream>
+#include <string>
+#include <cstddef>
 
-
-std::string hackerrankInString(std::string s)
+const std::string hackerrankInString(std::string& str) noexcept
 {
-   unsigned int index=0;
-   std::string hack="hackerrank";
+	const std::string hack{ "hackerrank" };
+	if (str.empty() || str.size() < hack.size())
+		return  std::string{ "NO" };
 
-   if(hack.length() > s.length())
-      return "NO";
-   else
-   {
-      for(unsigned int i=0; i< s.length(); ++i)
-         if(index < hack.length() && hack.at(index)==s.at(i))
-            ++index;
-      if(hack.length() == index)
-         return "YES";
-      else
-         return "NO";
-   }
+	std::size_t index{ 0 };
+	for (const char charector : str)
+		if (index < hack.size() && hack[index] == charector)
+			++index;
+
+	return  hack.size() == index ? std::string{ "YES" } : std::string{ "NO" };
 }
-//**************************************************
+
 int main()
 {
-   unsigned int q; //the number of queries
-   std::cin >> q;
-   if(q>=2 && q<=100)
-   {
-      for(int a0 = 0; a0 < q; a0++)
-      {
-         std::string s; //std::string s to be evaluated.
-         std::cin >> s;
-         std::string result = hackerrankInString(s);
-         std::cout << result << std::endl;
-      }
-   }
-   return 0;
+	std::size_t q{}; std::cin >> q; // queries
+	while (q--)
+	{
+		std::string str{}; // std::string s to be evaluated.
+		std::cin >> str;
+		std::cout << hackerrankInString(str) << '\n';
+	}
+	return 0;
 }
