28e19a99824d26e26d6b165f710dafdbe33c3ccf
Somewhere on Earth <email2jejo@gmail.com>
2019-05-14 14:41:51 +0200

    Algo - Mars Exploration: updates

--- AlgorithmsStrings - Mars Exploration/AlgorithmsStrings - Mars Exploration.cpp
+++ AlgorithmsStrings - Mars Exploration/AlgorithmsStrings - Mars Exploration.cpp
@@ -1,43 +1,22 @@
 #include <iostream>
-#include <algorithm>
+#include <string>
 
+using ushort = unsigned short;
 
 int main()
 {
-   std::string S;
-   std::cin>>S;
-   unsigned short Len=S.length(), Ans=0;
-   if(Len>=1 && (Len<=99 && Len%3==0))
-   {
-      for(unsigned short i=0; i<Len; i=i+3)
-      {
-         if(S.at(i)!='S')
-            ++Ans;
-         if(S.at(i+1)!='O')
-            ++Ans;
-         if(S.at(i+2)!='S')
-            ++Ans;
-         //std::cout<< "loop worked now"<<std::endl;
-      }
-   }
-   std::cout<<Ans<<std::endl;
-    return 0;
+	std::string str; std::cin >> str;
+	ushort ans{ 0 };
+
+	for (ushort i = 0; i < str.size() - 2; i += 3)
+	{
+		if (str[i] != 'S')
+			++ans;
+		if (str[i + 1] != 'O')
+			++ans;
+		if (str[i + 2] != 'S')
+			++ans;
+	}
+	std::cout << ans << '\n';
+	return 0;
 }
-// or
-/*
-int main(){
-    std::string s;
-   char te[3];
-    te[0]='S';
-    te[1]='O';
-    te[2]='S';
-    std::cin >> s;int i=0,j=0,count=0;;
-    while(s[i])
-        {   if(j==3)j=0;
-            if((s[i])!= (te[j]))count++;
-            i++;j++;
-        }
-    std::cout<<count;
-    return 0;
-}
-*/
